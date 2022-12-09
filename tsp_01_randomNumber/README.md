f0b1b34c26f25655bf351c6b2eb780b97919705e
Rida R Syed <ridarafisyed@yahoo.com>
2022-12-09 20:45:46 +0500

    update the project

---
+++ tsp_01_randomNumber/README.md
@@ -0,0 +1,26 @@
+<!-- @format -->
+
+# TypeScript-project01_Random Number Generated
+
+Here is a simple cli based calculator which can perform basic calculator
+operation. it takes two values from user and perform the following task
+
+<ul>
+<li>+ addition</li> 
+<li>- substraction</li> 
+<li>x multiplication</li> 
+<li>÷ division</li> 
+<li>% persentage</li> 
+<li>^ power</li> 
+<li>√ square root</li>
+<li>3√ cube root</li> 
+<li>sin </li> 
+<li>cos </li> 
+<li>tan </li> 
+<li>π </li> 
+<li>log </li>
+<li>e </li>
+
+</ul>
+
+![screen shot of terminal](./screenshot/image1.png)
--- tsp_01_randomNumber/src/index.ts
+++ tsp_01_randomNumber/src/index.ts
@@ -1,1 +1,64 @@
 /** @format */
+import inquirer from "inquirer";
+
+let welcome: string = "Welcome To the Number Guessing Game \n\n\n";
+type Guess = {
+	guess: number;
+};
+
+let checkConfirm: boolean = false;
+
+let result: number = 0;
+let attempt: number = 0;
+
+console.log(welcome);
+
+do {
+	let round: number = 0;
+	attempt += 1;
+	const numToGuess: number = Math.floor(Math.random() * 10) + 1;
+
+	function validateNumber(input: number): string | boolean {
+		if (isNaN(input)) {
+			return "Invalid Number!";
+		} else {
+			return true;
+		}
+	}
+
+	do {
+		console.log(`Game # ${attempt} - Round # ${round} \n\n`);
+		let input: Guess = await inquirer.prompt([
+			{
+				type: "number",
+				name: "guess",
+				message: "Enter your Guess",
+				validate: validateNumber,
+			},
+		]);
+		const userGuess = Number(input.guess);
+
+		if (userGuess < numToGuess) {
+			console.log(`\n Your Guess is smaller \n`);
+		} else if (userGuess > numToGuess) {
+			console.log(`\n Your Guess is greater \n `);
+		} else {
+			console.log(`\n Wow! you guessed the number \n`);
+			result += 1;
+		}
+
+		round += 1;
+	} while (round < 3);
+
+	console.log(`Your socre is ${result} / ${attempt} \n\n`);
+	console.log(`The Game is Over \n\n\n`);
+
+	const isConfirm = await inquirer.prompt([
+		{
+			name: "confirm",
+			type: "confirm",
+			message: "Do you want to try again",
+		},
+	]);
+	checkConfirm = isConfirm.confirm;
+} while (checkConfirm);
