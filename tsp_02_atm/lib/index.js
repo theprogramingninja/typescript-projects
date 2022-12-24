9f26ca290902c518e4151a486d0c64b2e8ea0376
Rida R Syed <ridarafisyed@yahoo.com>
2022-12-24 14:29:42 +0500

    update project repo

--- tsp_02_atm/lib/index.js
+++ tsp_02_atm/lib/index.js
@@ -1,3 +1,66 @@
+/** @format */
+import inquirer from "inquirer";
+const id = 112233445566;
+const pin = 1234;
 let welcome = "Welcome To the RRS ATM  \n\n\n";
-console.log(welcome);
-export {};
+let balance = Math.random() * 100;
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "user_id",
+        message: "Enter your id # : ",
+        // validate: validateNumber,
+    },
+    {
+        type: "password",
+        name: "user_pin",
+        message: "Enter your pin # : ",
+        // validate: validateNumber,
+    },
+]);
+const userId = Number(answers.user_id);
+const userPin = Number(answers.user_pin);
+if (userId === id && userPin === pin) {
+    answers.user_name = "Rida";
+    answers.user_balance = Math.floor(Math.random() * 1000);
+    console.log(`Hello ${answers.user_name}:`);
+    const choice = await inquirer.prompt([
+        {
+            type: "list",
+            choices: ["Balance Inquiry", "Transfer Amount", "Withdraw,"],
+            name: "functions",
+            message: "Choose operation",
+        },
+    ]);
+    switch (choice.functions) {
+        case "Balance Inquiry":
+            console.log(`Your current balance is :${answers.user_balance}`);
+            break;
+        case "Transfer Amount":
+            const transfer = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "account",
+                    message: "please enter the account number : ",
+                },
+                {
+                    type: "input",
+                    name: "amount",
+                    message: "Please enter the amount to transfer : ",
+                },
+            ]);
+            console.log(`balance before transfer : ${answers.user_balance}`);
+            if (answers.user_balance > transfer.amount) {
+                answers.user_balance -= transfer.amount;
+                console.log(`Your balance after transfer : ${answers.user_balance}`);
+            }
+            else
+                console.log(`Sorry, your balance is insufficent.`);
+        case "Withdraw":
+            break;
+        default:
+            break;
+    }
+}
+else
+    console.log("Not authenticated");
--- tsp_02_atm/src/index.ts
+++ tsp_02_atm/src/index.ts
@@ -1,10 +1,15 @@
 /** @format */
 import inquirer from "inquirer";
 
+const id: number = 112233445566;
+const pin: number = 1234;
+
 let welcome: string = "Welcome To the RRS ATM  \n\n\n";
 type Authentication = {
 	user_id: number;
 	user_pin: number;
+	user_name?: string;
+	user_balance?: number;
 };
 
 let balance: number = Math.random() * 100;
@@ -12,15 +17,71 @@ const answers: Authentication = await inquirer.prompt([
 	{
 		type: "input",
 		name: "user_id",
-		message: "Enter first number",
+		message: "Enter your id # : ",
 		// validate: validateNumber,
 	},
 	{
-		type: "input",
+		type: "password",
 		name: "user_pin",
-		message: "Enter second number",
+		message: "Enter your pin # : ",
 		// validate: validateNumber,
 	},
 ]);
 const userId = Number(answers.user_id);
 const userPin = Number(answers.user_pin);
+
+if (userId === id && userPin === pin) {
+	answers.user_name = "Rida";
+	answers.user_balance = Math.floor(Math.random() * 1000);
+	console.log(`Hello ${answers.user_name}:`);
+	const choice = await inquirer.prompt([
+		{
+			type: "list",
+			choices: ["Balance Inquiry", "Transfer Amount", "Withdraw,"],
+			name: "functions",
+			message: "Choose operation",
+		},
+	]);
+	switch (choice.functions) {
+		case "Balance Inquiry":
+			console.log(`Your current balance is :${answers.user_balance}`);
+			break;
+		case "Transfer Amount":
+			const transfer = await inquirer.prompt([
+				{
+					type: "input",
+					name: "account",
+					message: "please enter the account number : ",
+				},
+				{
+					type: "input",
+					name: "amount",
+					message: "Please enter the amount to transfer : ",
+				},
+			]);
+			console.log(`balance before transfer : ${answers.user_balance}`);
+			if (answers.user_balance > transfer.amount) {
+				answers.user_balance -= transfer.amount;
+				console.log(`Your balance after transfer : ${answers.user_balance}`);
+			} else console.log(`Sorry, your balance is insufficent.`);
+			break;
+
+		case "Withdraw":
+			const withdraw = await inquirer.prompt([
+				{
+					type: "input",
+					name: "amount",
+					message: "Please enter the amount to transfer : ",
+				},
+			]);
+			console.log(`balance before transfer : ${answers.user_balance}`);
+			if (answers.user_balance > withdraw.amount) {
+				answers.user_balance -= withdraw.amount;
+				console.log(`Your balance after withdraw : ${answers.user_balance}`);
+			} else console.log(`Sorry, your balance is insufficent.`);
+			break;
+
+		default:
+			break;
+	}
+} else console.log("Not authenticated");
